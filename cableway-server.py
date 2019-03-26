3aa744137f4ee6f802c739b8aab5a409c558f8a3
Stefan Stettler <stefan.stettler@noser.com>
2019-03-26 14:00:02 +0100

    Port 30123

--- cableway-server.py
+++ cableway-server.py
@@ -8,4 +8,9 @@ class CablewayController(object):
 
 
 if __name__ == '__main__':
+    cherrypy.config.update(
+        {
+            'server.socket_host': '0.0.0.0',
+            'server.socket_port': 30123,
+        })
     cherrypy.quickstart(CablewayController())
