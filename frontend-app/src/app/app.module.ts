ce2a1b6e3b6c8557a929915631657f9568ff4b71
Stefan Stettler <stefan.stettler@noser.com>
2019-03-27 16:23:42 +0100

    Angular Material added

--- frontend-app/src/app/app.module.ts
+++ frontend-app/src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
