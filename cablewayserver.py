f725ee1d4039223553958cd76149f0ac026c09ce
Stefan Stettler <stefan.stettler@noser.com>
2019-04-04 12:26:16 +0200

    Automatic stop implemented

--- cablewayserver.py
+++ cablewayserver.py
@@ -1,10 +1,12 @@
 import cherrypy
 from subprocess import call
+import threading
 
 
 class CablewayServer(object):
     def __init__(self, motorController):
         self.motor = motorController
+        self.timer = None
 
     @cherrypy.expose
     def status(self):
@@ -12,14 +14,22 @@ class CablewayServer(object):
 
     @cherrypy.expose
     def startLeft(self):
+        self.startTimer()      
         self.motor.turnLeft()
 
     @cherrypy.expose
     def startRight(self):
+        self.startTimer()    
         self.motor.turnRight()      
 
     @cherrypy.expose
     def stop(self):
+        print('Stop called')
+        if (self.timer != None):
+            if (self.timer.is_alive() == True):
+                self.timer.cancel()
+            self.timer = None
+        
         self.motor.stop()
 
     @cherrypy.expose
@@ -33,3 +43,12 @@ class CablewayServer(object):
     @cherrypy.expose
     def shutdown(self):
         call("sudo shutdown -h now", shell=True)    
+
+    
+    def startTimer(self):
+        if (self.timer == None):
+            self.timer = threading.Timer(5.0, self.stop)
+            self.timer.start()
+        else:
+            if (self.timer.is_alive() == False):
+                self.timer.start()
